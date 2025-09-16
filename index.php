7565308a05e19796559072d52a8c8ba360118b7c
zivannoerico <fanoerico@gmail.com>
2025-09-16 10:00:29 +0700

    pushagain

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
 <?php include "component/header.php" ?> 
 </head>
 <body>
     <?php include "component/navbar.php" ?> 
-    <?php include "component/section.php" ?> 
+    <?php include "page/home.php" ?> 
     <?php include "component/footer.php" ?> 
 </body>
+</html>
